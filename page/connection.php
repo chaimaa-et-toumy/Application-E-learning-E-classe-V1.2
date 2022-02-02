44e1b46e586783547f9b09c99714e7fe55c069cc
chaimaa et-toumy <chaimaetoumy5@gmail.com>
2022-02-02 22:16:10 +0100

    connexion a une base de donne MySQL -PDO-

--- page/connection.php
+++ page/connection.php
@@ -10,7 +10,8 @@ try {
   // set the PDO error mode to exception
   $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
   // echo "Connected successfully";
-} catch(PDOException $e) {
+} 
+catch(PDOException $e) {
   echo "Connection failed: " . $e->getMessage();
 }
 ?>
