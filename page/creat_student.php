356dfd03e5ef675f9949809f2d3bba7f0dbd1f01
chaimaa et-toumy <chaimaetoumy5@gmail.com>
2022-02-03 11:47:48 +0100

    ajouter responsive sur page create student

--- page/creat_student.php
+++ page/creat_student.php
@@ -1,6 +1,6 @@
 <?php
  include 'connection.php';
-    if(isset($_POST['create'])){
+if(isset($_POST['create'])){
     $Name = $_POST['Name'];
     $Email = $_POST['Email'];
     $Phone = $_POST['Phone'];
@@ -19,6 +19,8 @@
     <title>E-Classe | Creat Student</title>
     <meta name="keywords" content="YouCode,Youssoufia,E-Classe">
     <meta name="description" content="application web pour les étudiants de YouCode">
+    <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css"
+        integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous" />
     <!-- CSS only -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
@@ -26,9 +28,13 @@
 </head>
 
 <body class="bg-body">
-    <main class="container d-flex  justify-content-center  align-items-center vh-100">
-        <form class="w-50 card p-4" method="POST" action="#">
+    <main class="container-fluid creat_student">
+        <div class="d-flex align-items-center justify-content-center vh-100">
+        <form class="form-classe card p-4" method="POST" action="#">
+            <div class="d-flex justify-content-center">
+            <a href="../student.php"><i class="fas fa-backward pe-5"></i></a>
             <p class="text-uppercase h4 text-center fw-bold"> Creat student </p>
+            </div>
             <div class="mb-3">
                 <label class="form-label">Name</label>
                 <input type="text" class="form-control" name="Name"  placeholder="Entre your name" required>
@@ -52,6 +58,7 @@
 
             <button type="submit" class="btn btn-primary w-100" name="create">create</button>
         </form>
+        </div>
     </main>
     <script src="js/bootstrap.bundle.min.js"></script>
 </body>
