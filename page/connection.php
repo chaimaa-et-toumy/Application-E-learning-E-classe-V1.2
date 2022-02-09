72c67b9459ed123b53cf811b55df4e644267990d
Chaimaa ET-Toumy <chaimaetoumy5@gmail.com>
2022-02-09 10:58:38 +0100

    Update connection.php

--- page/connection.php
+++ page/connection.php
@@ -8,7 +8,6 @@ $password = "A1Z2E3R4T5Y6"; // password of this user
 try {
   $conn = new PDO($dataname, $username, $password); //start connection with pdo
   // set the PDO error mode to exception
-  $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
   // echo "Connected successfully";
 } catch(PDOException $e) {
   echo "Connection failed: " . $e->getMessage();
